702c4ba202d173a965b0c5f891fac1793fdda383
Fatima Kashif <149135030+quicklearner404@users.noreply.github.com>
2025-03-29 00:29:17 +0500

    Create rating.js

---
+++ backend/routes/rating.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const {
+  addRating,
+  getRatingsByUser,
+  getRatingsByMovie,
+  getAverageRating
+} = require('../controllers/ratingController');
+
+// Add new rating
+router.post('/', addRating);
+
+// Get all ratings by user
+router.get('/user/:userId', getRatingsByUser);
+
+// Get all ratings for a movie
+router.get('/movie/:movieId', getRatingsByMovie);
+
+// Get average rating for a movie
+router.get('/movie/:movieId/average', getAverageRating);
+
+module.exports = router;
