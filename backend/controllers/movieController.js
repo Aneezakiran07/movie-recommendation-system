930be4b83feb58d50ec65f2bb63c0d22a8922075
Asharr-Ali <asharalihabib2003@gmail.com>
2025-04-14 23:39:38 +0500

    Just changed only vote_average to ratings

--- backend/controllers/movieController.js
+++ backend/controllers/movieController.js
@@ -19,7 +19,7 @@ const getTopRatedMovies = async (req, res) => {
   try {
     const pool = await connectToDB();
     const result = await pool.request().query(`
-      SELECT TOP 100 * FROM Movies ORDER BY vote_average DESC 
+      SELECT TOP 100 * FROM Movies ORDER BY ratings DESC 
     `);
     res.json(result.recordset);
   } catch (err) {
